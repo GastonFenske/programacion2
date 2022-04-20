3b732a2e3c1e59d5364947824840c74f8a957c5a
Gaston Fenske <gastonfenske@MacBook-Pro-de-Gaston.local>
2022-04-20 18:13:06 -0300

    arreglando un poco mas el codigo

--- test/src/com/company/Menu.java
+++ test/src/com/company/Menu.java
@@ -7,6 +7,7 @@ public class Menu {
 
     private final String MENU_MESSAGE_OP = "Ingrese un operador (+, -, *, /, =): ";
     private final String MENU_MESSAGE_NUMBERS = "Ingrese un numero: ";
+    private final String MENU_MESSAGE_ERROR = "No es posible divir por cero, se mostrara el resultado hasta donde fue posible \n";
 
     public void show(String message){
         System.out.print(message);
@@ -26,13 +27,17 @@ public class Menu {
         return operator;
     }
 
-
     public void calc(List<Integer> numbers, List<String> operators){
         Integer result = numbers.get(0);
         List<Integer> numbersToSend = new ArrayList<Integer>();
         numbersToSend.add(numbers.remove(0));
         for (String operator : operators) {
-            numbersToSend.add(numbers.remove(0));
+            Integer getNumber = numbers.remove(0);
+            if (getNumber == 0) {
+                this.show(MENU_MESSAGE_ERROR);
+                break;
+            }
+            numbersToSend.add(getNumber);
             result = this.processOperation(operator, numbersToSend);
             numbersToSend.clear();
             numbersToSend.add(result);
